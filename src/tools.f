2923a4a980bcbf9e0ed17107bfc1ef4259607807
Tim Moorhouse <timmoorhouse@gmail.com>
2023-02-24 08:23:06 -0400

    early prep for dump

--- src/tools.f
+++ src/tools.f
@@ -1,4 +1,24 @@
 
+: dump ( addr u -- )
+
+    2dup 16 min \ ( addr u addr u2 )
+    over . 
+
+    \ 2dup over + swap do i c@ . loop
+
+    space
+    char | emit
+    space
+
+    \ 2dup over + swap do i c@ emit loop
+
+    cr
+    2drop
+
+    2drop ;
+
+: see ( "<spaces>name" -- ) ;
+
 \ TODO traverse-namelist should do the hidden check
 \ : print-name ( nt -- u ) dup ?hidden =0 if
 \         out @ 
