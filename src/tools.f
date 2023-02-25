4f952d2f9ac3466006196cb2aee604dcfc769594
Tim Moorhouse <timmoorhouse@gmail.com>
2023-02-25 16:41:30 -0400

    move words to forth

--- src/tools.f
+++ src/tools.f
@@ -39,15 +39,16 @@
         cr
     then r> base ! ;
 
-\ : print-name ( nt -- u ) dup ?hidden =0 if
-\         out @ 
-\             dup if space then
-\             c/l greater if cr then
-\         name>string type
-\     then 
-\     true
-\ ;
-\ 
-\ : words ( -- ) ' print-name forth-wordlist traverse-wordlist ;
+: id. ( nt -- ) name>string type ;
+
+\ TODO use a :noname ...
+: print-name ( nt -- u ) 
+    out c@ 
+    dup if space then
+    c/l > if cr then
+    id.
+    true ;
+
+: words ( -- ) ['] print-name forth-wordlist traverse-wordlist ;
 
 .( end of tools.f )
