5448c4fb0b9cf89f6764a118908c1c2681f47eea
Tim Moorhouse <timmoorhouse@gmail.com>
2023-03-02 20:42:51 -0400

    add [DEFINED], [UNDEFINED]

--- src/file-ext.f
+++ src/file-ext.f
@@ -3,6 +3,16 @@
 \
 \ FILE-STATUS FLUSH-FILE INCLUDE RENAME-FILE REQUIRE REQUIRED
 
+\ TODO file-status
 
+\ TODO flush-file
+
+\ TODO : include ( "<spaces>name" -- ) parse-name included ;
+
+\ TODO rename-file
+
+\ TODO require
+
+\ TODO required
 
 .( ... end of file-ext.f ) cr
