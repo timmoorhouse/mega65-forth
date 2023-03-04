b9437778e336186d8993b004b756cb578d9410d7
Tim Moorhouse <timmoorhouse@gmail.com>
2023-03-04 17:43:43 -0400

    tweaking runtests.f

--- src/runtests.f
+++ src/runtests.f
@@ -11,21 +11,21 @@ include tester.fr
 \ S" ttester.fs" INCLUDED
 \ true verbose !
 
-include core.fr
-include coreplustest.fth
-include utilities.fth
-include errorreport.fth
-include coreexttest.fth
-\ S" blocktest.fth" INCLUDED
-\ include doubletest.fth
-include exceptiontest.ft            \ note file name truncation
-\ S" facilitytest.fth" INCLUDED
-\ S" filetest.fth" INCLUDED
-\ S" localstest.fth" INCLUDED
-\ S" memorytest.fth" INCLUDED
-\ S" toolstest.fth" INCLUDED
-\ S" searchordertest.fth" INCLUDED
-\ S" stringtest.fth" 
+s" core.fr" included
+s" coreplustest.fth" included
+s" utilities.fth" included
+s" errorreport.fth" included
+s" coreexttest.fth" included
+\ s" blocktest.fth" included
+\ s" doubletest.fth" included
+s" exceptiontest.ft" included            \ note file name truncation
+\ s" facilitytest.fth" included
+\ s" filetest.fth" included
+\ s" localstest.fth" included
+\ s" memorytest.fth" included
+\ s" toolstest.fth" included
+\ s" searchordertest." included
+\ s" stringtest.fth" included
 
 include internalstest.f
 
