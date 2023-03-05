bd99838279e7c90287425e0f543555807734fffe
Tim Moorhouse <timmoorhouse@gmail.com>
2023-03-05 12:42:22 -0400

    add UNESCAPE

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,11 @@
 
 v0.2 alpha TBD
+- Add COMPILE-ONLY
+- Catch exceptions in autoboot.f
 - CORE
-  - Added ENVIRONMENT?
+  - Add ENVIRONMENT?
+- STRING
+  - Add UNESCAPE
 
 v0.1 pre-alpha 2023-03-04
 - Initial version
--- src/string.f
+++ src/string.f
@@ -11,4 +11,17 @@
 
 \ SLITERAL see core.f
 
+\ Replace each '%' character in the input string c-addr1 len1 with two '%' characters.
+\ The output is represented by c-addr2 len2.
+\ If you pass a string through UNESCAPE and then SUBSTITUTE, you get the original string.
+: unescape ( c-addr1 len1 c-addr2 -- c-addr2 len2 )
+   dup 2swap over + swap ?do
+     i c@ [char] % = if
+       [char] % over c! 1+
+     then
+     i c@ over c! 1+
+   loop
+   over -
+;
+
 .( ... end of string.f ) cr
