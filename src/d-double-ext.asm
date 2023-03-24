210271b06efb248a0fb9b75c8d25d4af41326342
Tim Moorhouse <timmoorhouse@gmail.com>
2023-03-24 12:45:52 -0300

    add 2ROT

--- src/d-double-ext.asm
+++ src/d-double-ext.asm
@@ -7,14 +7,34 @@
 ; (x1 x2 x3 x4 x5 x6 -- x3 x4 x5 x6 x1 x2)
 
 !if ENABLE_DOUBLE_EXT {
-}
-
-; ****************************************************************************
-; 2VALUE
-
-; See reference implementation
-
-!if ENABLE_DOUBLE_EXT {
+        +CREATE "2rot", 0
+W_2ROT
+        !word *+2
+        ldy 0,x
+        lda 8,x
+        sta 0,x
+        lda 4,x
+        sta 8,x
+        sty 4,x
+        ldy 2,x
+        lda 10,x
+        sta 2,x
+        lda 6,x
+        sta 10,x
+        sty 6,x
+        ldy 1,x
+        lda 9,x
+        sta 1,x
+        lda 5,x
+        sta 9,x
+        sty 5,x
+        ldy 3,x
+        lda 11,x
+        sta 3,x
+        lda 7,x
+        sta 11,x
+        sty 7,x
+        jmp NEXT
 }
 
 ; ****************************************************************************
--- src/d-double-ext.f
+++ src/d-double-ext.f
@@ -1,8 +1,6 @@
 
 \ The following words are implemented internally:
-\ DU<
-
-\ TODO 2ROT
+\ 2ROT DU<
 
 : 2value ( x1 x2 "<spaces>name" -- ) create , , 
   does> store? @ if 2! 0 store? ! else 2@ then ;
